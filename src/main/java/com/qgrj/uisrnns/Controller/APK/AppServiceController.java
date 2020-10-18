1305e495af94dd485259fd06260a137664abde76
wuzusong <wzy@zhgt999.com>
2020-10-18 22:28:00 +0800

    添加 APP第一屏查找接口

---
+++ src/main/java/com/qgrj/uisrnns/Controller/APK/AppServiceController.java
@@ -0,0 +1,36 @@
+package com.qgrj.uisrnns.Controller.APK;
+
+import com.qgrj.uisrnns.Dao.CntDatatablesMapper;
+import com.qgrj.uisrnns.Pojo.CntDatatables;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/app")
+public class AppServiceController {
+
+    @Autowired
+    CntDatatablesMapper cntDatatablesMapper;
+
+    @GetMapping("/test")
+    public String test(){ return "test";}
+
+    // 获取今天总流量
+    @GetMapping("/getTodayNum")
+    public Integer getTodayNum(){ return cntDatatablesMapper.getTodayNum();}
+
+    // 获取昨天总流量
+    @GetMapping("/getYesterdayNum")
+    public Integer getYesterdayNum(){ return cntDatatablesMapper.getYesterdayNum();}
+    // 流量纪录
+    @GetMapping("/flowRecord")
+    public Integer flowRecord(){ return cntDatatablesMapper.flowRecord();}
+    // 近7天的查询总数据
+    @GetMapping("/getLastSevenDayInfo")
+    public List<CntDatatables> getLastSevenDayInfo(){ return cntDatatablesMapper.getLastSevenDayInfo();}
+
+}
--- src/main/java/com/qgrj/uisrnns/Dao/CntDatatablesMapper.java
+++ src/main/java/com/qgrj/uisrnns/Dao/CntDatatablesMapper.java
@@ -4,17 +4,33 @@ package com.qgrj.uisrnns.Dao;
 
 import com.qgrj.uisrnns.Pojo.CntDatatables;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 @Mapper
 public interface CntDatatablesMapper {
     int deleteByPrimaryKey(Integer uid);
-
     int insert(CntDatatables record);
-
     CntDatatables selectByPrimaryKey(Integer uid);
-
     List<CntDatatables> selectAll();
-
     int updateByPrimaryKey(CntDatatables record);
+
+
+    // 昨天流量
+    @Select("SELECT sum(PeopleNum) as num FROM cnt_datatables WHERE (TO_DAYS(NOW()) - TO_DAYS(cnt_datatables.Date)  )= 1")
+    public Integer getYesterdayNum();
+
+    // 今天流量
+    @Select("SELECT sum(PeopleNum) FROM cnt_datatables WHERE TO_DAYS(cnt_datatables.Date) = TO_DAYS(NOW())")
+    public Integer getTodayNum();
+
+    // 最高流量纪录
+    @Select("SELECT max(PeopleNum) as num FROM cnt_datatables")
+    public Integer flowRecord();
+
+    //近七天的数据
+    @Select("SELECT * FROM cnt_datatables WHERE (TO_DAYS(NOW()) - TO_DAYS(cnt_datatables.Date)  )<= 7")
+    public List<CntDatatables> getLastSevenDayInfo();
+
+
 }
